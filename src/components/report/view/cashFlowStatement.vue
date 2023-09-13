7e07ddefe182011daed291064818ad592e0d9767
Pankaj Dudhat <pankaj@mobileideabox.com>
2023-09-13 17:08:58 +0530

    Add cash flow statement view repot

---
+++ src/components/report/view/cashFlowStatement.vue
@@ -0,0 +1,173 @@
+<template>
+    <b-card>
+        <div class="report-table">
+        <table class="w-100">
+            <tr>
+                <td class="w-100">
+                    <table class="w-100">
+                        <tr class="w-100">
+                            <td class="align-top">
+                                <h4>Cash Flow Statement</h4>
+                                <p>Sabah Softwoods</p>
+                                <p>Period Ending 31/01/23</p>
+                                <div class="mt-3">
+                                    <p>All Amounts are in RM</p>
+                                    <p>Start date 01/01/23</p>
+                                </div>
+                            </td>
+                            <td class="text-end align-top">
+                                <p>8/9/2023</p>
+                                <p>Page: 1/1</p>
+                                <p>SWOODPAMSUB</p>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td class="w-100">
+                    <table class="w-100 balance-table">
+                        <tr class="w-100">
+                            <th class="border-bottom">Description</th>
+                            <th class="border-bottom">Net Changes</th>
+                        </tr>
+                        <tr class="w-100">
+                            <td class="pt-2">
+                                <b>Operation Activities</b>
+                            </td>
+                            <td></td>
+                        </tr>
+                        <tr class="w-100">
+                            <td class="ps-2">
+                                Net Income
+                            </td>
+                            <td>2970,650.43</td>
+                        </tr>
+                        <tr class="w-100">
+                            <td class="ps-2">
+                                Adjutments to reconcile Net Income
+                            </td>
+                            <td></td>
+                        </tr>
+                        <tr class="w-100">
+                            <td class="ps-2">
+                                Account Receivables
+                            </td>
+                            <td>890,596.41</td>
+                        </tr>
+                        <tr class="w-100">
+                            <td class="ps-2">
+                                Prepaid Expenses
+                            </td>
+                            <td>
+                                <b></b>
+                            </td>
+                        </tr>
+                        <tr class="w-100">
+                            <td class="ps-2">
+                                Inventory
+                            </td>
+                            <td>1,449,796.0</td>
+                        </tr>
+                        <tr class="w-100">
+                            <td class="ps-2">
+                                Currtent Liabilities
+                            </td>
+                            <td></td>
+                        </tr>
+                        <tr class="w-100">
+                            <td class="ps-2">
+                                Payroll Liabilities
+                            </td>
+                            <td class="border-bottom"></td>
+                        </tr>
+                        <tr class="w-100">
+                            <td>
+                                <b>Net Cash Provided by Operating Activities</b>
+                            </td>
+                            <td>
+                                <b>1,476,258.42</b>
+                            </td>
+                        </tr>
+                        <tr class="w-100">
+                            <td>
+                                <b>Investing Activities</b>
+                            </td>
+                            <td>
+                                
+                            </td>
+                        </tr>
+                        <tr class="w-100">
+                            <td class="ps-2">
+                                Equipment
+                            </td>
+                            <td></td>
+                        </tr>
+                        <tr class="w-100">
+                            <td class="ps-2">
+                                Accumulated Desrciption
+                            </td>
+                            <td class="border-bottom"></td>
+                        </tr>
+                        <tr class="w-100">
+                            <td>
+                                <b>Net Cash Provided by Investing Activities</b>
+                            </td>
+                            <td>
+                                <b></b>
+                            </td>
+                        </tr>
+                        <tr class="w-100">
+                            <td>
+                                <b>Financing Activities</b>
+                            </td>
+                            <td>
+                                
+                            </td>
+                        </tr>
+                        <tr class="w-100">
+                            <td class="ps-2">
+                                Long Term Liabilities
+                            </td>
+                            <td></td>
+                        </tr>
+                        <tr class="w-100">
+                            <td class="ps-2">
+                                Distribution to Shareholders
+                            </td>
+                            <td class="border-bottom"></td>
+                        </tr>
+                        <tr class="w-100">
+                            <td>
+                                <b>Net Cash Provided by Distribution</b>
+                            </td>
+                            <td>
+                                <b></b>
+                            </td>
+                        </tr>
+                        <tr class="w-100">
+                            <td class="ps-2">
+                                Net Cash Increase for the Period
+                            </td>
+                            <td>122,585.55</td>
+                        </tr>
+                        <tr class="w-100">
+                            <td class="ps-2">
+                                Cash at Beginning of the Period
+                            </td>
+                            <td class="border-bottom"></td>
+                        </tr>
+                        <tr class="w-100">
+                            <td class="ps-2">
+                                <b>Cash at End of the Period</b>
+                            </td>
+                            <td class="border-bottom"><b>1,476,258.54</b></td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+    </div>
+  </b-card>
+   
+</template>
